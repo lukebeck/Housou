55a2d7a1c2b5a696ab180363a732b11b0a8ede23
Luke <lukembeck@gmail.com>
2018-08-08 11:23:52 +1000

    Reformated driver

---
+++ housou/driver.py
@@ -0,0 +1,64 @@
+import csv
+from lxml import html
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+class Driver:
+    '''Chrome driver object for web scraping'''
+    def __init__(self,path):
+        '''Set driver path and options and initialise driver'''
+        print('Initialising driver')
+        self.path = path
+        self.options = self.setOptions()
+        self.driver = webdriver.Chrome(
+            chrome_options=self.options,
+            executable_path=self.path)
+
+    def setOptions(self):
+        '''Return options for headless chrome driver'''
+        print('Setting options')
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('disable-infobars')
+        options.add_argument('--disable-extensions')
+        return options
+
+    def getTree(self,url):
+        '''Return HTML tree from given url'''
+        self.driver.get(url)
+        print('Extracting tree from url')
+        innerHTML = self.driver.execute_script('return document.body.innerHTML')
+        return html.fromstring(innerHTML)
+
+    def getElements(self,tree,_xpath):
+        '''Get a list of elements from specified xpath'''
+        print('Extracting elements from tree')
+        value = tree.xpath(_xpath)
+        return value
+
+    def getContent(self,tree,xpaths):
+        '''Extract content from specified xpaths'''
+        print('Extracting contents from tree')
+        entry = []
+        for key in xpaths:
+            value = self.concatenate(tree.xpath(xpaths[key]))
+            value = value.replace('\n', '\\n')
+            entry.append(value)
+        return entry
+
+    def tearDown(self):
+        '''Tear down driver'''
+        print('Tearing down driver')
+        self.driver.close()
+        self.driver.quit()
+
+    @staticmethod
+    def concatenate(x):
+        return '\t'.join([y.text_content() for y in x])
+
+    @staticmethod
+    def write(entry,csv_path):
+        with open(csv_path,'a') as file:
+            wr = csv.writer(file)
+            wr.writerow(entry)
--- housou/get_articles.py
+++ housou/get_articles.py
@@ -1,74 +1,54 @@
 import csv
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import os
-from lxml import html
+from . import driver
 import os
 
-file_path = os.path.dirname(os.path.realpath(__file__))
-csv_file = file_path + '/articles.csv'
+# file paths
+dir_path = os.path.dirname(os.path.realpath(__file__))
+csv_path = dir_path + '/articles.csv'
+driver_path = dir_path + '/chromedriver'
+
+## main NHK page ##
+# url
+main_url = 'https://www3.nhk.or.jp/news/catnew.html?utm_int=news_contents_news-main_more'
+# xpath
+links_xpath = '//div[@class="content--items"]/ul/li/a/@href'
+
+## Article pages ##
+# urls extracted in main()
+root_url = 'https://www3.nhk.or.jp/'
+# xpaths
+xpaths = {
+    'title': '//h1[@class="content--title"]',
+    'date': '//p[@class="content--date"]/time',
+    'content': '//section[@class="content--detail-main"]'}
+
+# Article titles in csv file
+# For duplicate checking
+titles = []
+with open(csv_path,'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        titles.append(row[0])
 
 def housou():
-
-    # Chromedriver
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('disable-infobars')
-    chrome_options.add_argument('--disable-extensions')
-    chrome_driver = file_path + '/chromedriver'
-
-    #Urls
-    url = 'https://www3.nhk.or.jp/news/catnew.html?utm_int=news_contents_news-main_more'
-    root_url = 'https://www3.nhk.or.jp/'
-
-    # Xpaths
-    link_path = '//div[@class="content--items"]/ul/li/a/@href'
-    content_path = '//section[@class="content--detail-main"]'
-    title_path = '//h1[@class="content--title"]/text()'
-    date_path = '//p[@class="content--date"]/time/text()'
-
-    # Get page links from NHK news homepage inner HTML
-    print('🔐 Extracing links...')
-    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
-    driver.get(url)
-    innerHTML = driver.execute_script("return document.body.innerHTML")
-    tree = html.fromstring(innerHTML)
-    links = tree.xpath(link_path)
-    print('🔓 Extracted!')
-
-    titles = []
-    with open(csv_file,'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            titles.append(row[0])
-
-    # scape linked article pages
+    # Initialise driver
+    d = driver.Driver(driver_path)
+    # Extract HTML tree from main NHK page
+    tree = d.getTree(main_url)
+    # Extract article links from main NHK page
+    links = d.getElements(tree,links_xpath)
+    # If unique, write article contents to csv
     for link in links:
-        driver.get(root_url + link)
-        innerHTML = driver.execute_script('return document.body.innerHTML')
-        tree = html.fromstring(innerHTML)
-        # Get content
-        title = tree.xpath(title_path)[0]
-        if title in titles:
-            print(f'㊢ 複写の記事 :: {title}')
-            print('     記事を飛びました')
+        url = root_url + link
+        tree = d.getTree(url)
+        entry = d.getContent(tree,xpaths)
+        if entry[0] in titles:
+            print(entry[0])
+            print('㊢ 複写の記事を飛びました')
             pass
         else:
-            content = tree.xpath(content_path)[0].text_content()
-            date = tree.xpath(date_path)[0]
-            # echo content to command line
-            print(f'リンク :: {link}')
-            print(f'     題名 :: {title}')
-            print(f'     日付 :: {date}')
-            print(f'     内容 :: {content[0:20]}...')
-            # Add to entries csv
-            entry = [
-                title,
-                date,
-                content]
-            with open(csv_file,'a') as file:
-                wr = csv.writer(file)
-                wr.writerow(entry)
-
-    driver.quit()
+            print(entry[0])
+            d.write(entry,csv_path)
+            print('㊣ データベースに記事を入力できました')
+    # Tear down the driver upon completion
+    d.tearDown()
